44e260949962e7d2ced68a0161023ecc9004201b
Maxwell Baird <bairdm71@gmail.com>
2020-06-08 09:56:20 -0600

    started making the service for finding a restaurant

--- app/poro/foodie.rb
+++ app/poro/foodie.rb
@@ -1,16 +1,21 @@
 class Foodie
 
   def initialize(start_point,end_point)
+    @coordinates = get_coordinates(end_point)
     @travel_time = find_duration(start_point,end_point)
     @end_location = end_point
-    @forecast = find_forecast(end_point)
+    @forecast = find_forecast(@coordinates)
+    @restaurant = find_restaurant(@coordinates)
   end
 
   private
 
-  def find_forecast(end_point)
+  def get_coordinates(end_point)
     geocode = GeocodingService.new
     geocode_response = geocode.coordinates(end_point)
+  end
+
+  def find_forecast(geocode_response)
     weather_service = WeatherService.new
     response = weather_service.one_call(geocode_response)
     forecast_parse(response)
@@ -24,10 +29,8 @@ class Foodie
   def forecast_parse(response)
     hash = Hash.new
     time_rounded_up = @travel_time[0].to_i + 2
-    if time_rounded_up < 48
-      hash[:temperature] = response[:hourly][time_rounded_up][:temp]
-      hash[:summary] =
-    else
-    end
+    hash[:temperature] = response[:current][:temp]
+    hash[:summary] = response[:current][:weather][0][:description]
+    hash
   end
 end
---
+++ app/services/zomato_service.rb
@@ -0,0 +1,9 @@
+class ZomatoService
+
+  def restaurant(location)
+    lat = location[:lat]
+    lon = location[:lng]
+    response = Faraday.get("https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{lon}&units=imperial&exclude=minutely&appid=#{ENV['WEATHER_API']}")
+    body = JSON.parse(response.body, symbolize_names: true)
+  end
+end
