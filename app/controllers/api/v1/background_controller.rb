d09d4f6c324da0c4801222ffd681c4ce9fcce1b1
Maxwell Baird <bairdm71@gmail.com>
2020-06-09 18:35:20 -0600

    added sad paths for endpoint 1

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -1,10 +1,25 @@
 class Api::V1::BackgroundController < ApplicationController
 
   def show
-    response = PhotosService.new
-    url = Hash.new
-    url[:url] = response.photo_url(params[:location])
-    render json: url
+    if params[:location].nil?
+      render json: {error: "One or more fields are blank"}, status: 401
+    else
+      if check_location_valid(params[:location])
+        response = PhotosService.new
+        url = Hash.new
+        url[:url] = response.photo_url(params[:location])
+        render json: url
+      else
+        render json: {error: "One or more locations are in an incorrect format"}, status: 402
+      end
+    end
+
+  end
+
+  private
+
+  def check_location_valid(location)
+    location.split(',').length == 2 && location.split(',')[1].length == 2
   end
 
 end
